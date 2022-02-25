f103e1333013730107cf92ea3f67e2c6098d365d
Tsogzolmaa <Tsogzolmaa@DESKTOP-955PGHS>
2022-02-25 12:44:09 +0800

    second

--- CircleCalc/src/Circle.java
+++ CircleCalc/src/Circle.java
@@ -11,7 +11,7 @@ public class Circle {
 	public static void main(String[] args) {
 		System.out.println("------------------------------------------------------------");
 		System.out.println("CircleCalc v1.0");
-		System.out.println();
+		System.out.println();//gg
 		System.out.println("Calculates and prints information for a user-supplied radius");
 		System.out.println("------------------------------------------------------------");
 		System.out.print("Enter the circle's radius: ");
