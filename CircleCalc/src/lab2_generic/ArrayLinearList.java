c1522fbcfcdfa2d66c25870b4a53ec5acb5fc20b
Tsogzolmaa <Tsogzolmaa@DESKTOP-955PGHS>
2022-03-25 06:16:09 +0800

    lab 2 - remove zasax

---
+++ CircleCalc/src/lab2_generic/ArrayLinearList.java
@@ -0,0 +1,61 @@
+package lab2_generic;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class ArrayLinearList<G> {
+
+	public int size = 0;
+	protected G[] elementData;
+
+	public int size() {
+		return size;
+	}
+
+	public void addElement(G[] ob) {
+		this.elementData = ob;
+		size = size + 1;
+	}
+
+	public void toString1() {
+		if (size != 0) {
+			for (int i = 0; i < elementData.length; i++) {
+				System.out.println(i + ". " + elementData[i]);
+			}
+		}
+		else {
+			System.out.println("Хоосон байна");
+		}
+	}
+
+	public int indexOf(int element) {
+		for (int i = 0; i < elementData.length; i++) {
+			if ((int) elementData[i] == element) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public G get(int indexE) {
+		G result = null;
+		for (int i = 0; i < elementData.length; i++) {
+			if (elementData[indexE].equals(elementData[i])) {
+				result = elementData[indexE];
+			}
+		}
+		return result;
+	}
+
+	
+
+	public void remove(int index) {
+		if (get(index) != null) {
+			for (int i = index + 1; i <= elementData.length; i++)
+				elementData[i - 1] = elementData[i];
+			//size = size - 1;
+			//elementData[--size] = null; // enable garbage collection
+		}
+	}
+
+}
---
+++ CircleCalc/src/lab2_generic/MainReturn.java
@@ -0,0 +1,80 @@
+package lab2_generic;
+
+import java.util.Scanner;
+
+public class MainReturn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayLinearList<Integer> list = new ArrayLinearList<Integer>();
+
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("1. isString\n2. Size\n3. Add\n4. IndexOf\n5. Delete\n6. Get");
+			System.out.println("Та сонголтоо оруулна уу?");
+			int songolt = sc.nextInt();
+			while (songolt != -1) {
+				switch (songolt) {
+				case 1:
+					list.toString1();
+					break;
+				case 2:
+					System.out.println("Массивын урт: " + list.size());
+					break;
+				case 3:
+					System.out.println("Массивын хэмжээ оруулна уу?");
+					int size = sc.nextInt();
+					Integer[] newarray = new Integer[size];
+					System.out.println("Массивын утга оруулна уу?");
+					for (int i = 0; i < newarray.length; i++) {
+						newarray[i] = sc.nextInt();
+					}
+					list.addElement(newarray);
+					break;
+				case 4:
+					System.out.println("Элемент оруулна уу?");
+					int searchElement = sc.nextInt();
+					int save = list.indexOf(searchElement);
+					if (save == -1) {
+						System.out.println("Энэ элемэнт массивт алга");
+					} else {
+						System.out.println(searchElement + " элемэнтийн индекс: " + list.indexOf(searchElement));
+					}
+					break;
+				case 5:
+					System.out.println("Устгах элемэнт оруулна уу?");
+					int deleteElement = sc.nextInt();
+					int saveIndex = list.indexOf(deleteElement);
+					if (saveIndex == -1) {
+						System.out.println("Энэ элемэнт массивт алга");
+					} else {
+						list.remove(deleteElement);
+						System.out.println("Амжилттай устгалаа");
+					}
+					break;
+				case 6:
+					System.out.println("Индеск оруулна уу?");
+					int searchIndex = sc.nextInt();
+					list.get(searchIndex);
+					if(list.get(searchIndex) == null) {
+						System.out.println("Уг индескт элемент алга байна");
+					}
+					else {
+						System.out.println(searchIndex + " дэх элемэнт нь " + list.get(searchIndex));
+					}
+					break;
+				case 7:
+					break;
+				default:
+					System.out.println("Та буруу тоо оруулсан байна");
+					break;
+				}
+				System.out.println("Дахин сонголт оруулна уу?");
+				songolt = sc.nextInt();
+			}
+		} catch (Exception e) {
+			System.out.println("ALDAATAI BAINA");
+		}
+
+	}
+
+}
